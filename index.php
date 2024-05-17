93acce603274ef228c16676e2d4cca7e44ec0e25
YukaParamitha <dewaayuoka741@gmail.com>
2024-05-17 18:00:33 +0800

    title baru diganti

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MOVIE wkwkkwkwk</title>
+    <title>MOVIE LIST</title>
     <!-- <link rel="stylesheet" href="styles.css"> -->
     <style>
         body {
